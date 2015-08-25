a38d3f842cea05522915ed51c4a5826221c7a4b2
gaurav <gaurav.mukherjee3@gmail.com>
2015-08-26 00:33:42 +0530

    next

--- dist/all.js
+++ dist/all.js
@@ -16,7 +16,29 @@ foodApp.controller('mapCtrl', ['$scope', '$http', 'uiGmapGoogleMapApi', function
 
     $scope.trips = [];
     $scope.getTrips = function(){
-        $http.get("https://hypertrack-api-staging.herokuapp.com/api/v1/gps/?trip_id=" + $scope.trip_id).success(function(data){
+        var url = "https://hypertrack-api-staging.herokuapp.com/api/v1/gps/?trip_id=" + $scope.trip_id;
+        setTrips(url)
+        //$http.get("https://hypertrack-api-staging.herokuapp.com/api/v1/gps/?trip_id=" + $scope.trip_id).success(function(data){
+        //    console.log(data);
+        //    data.results.forEach(function(v, i) {
+        //        v.pos = { latitude: v.location.coordinates[1], longitude: v.location.coordinates[0] }
+        //        v.events = {
+        //            click: function(a, b, c) {
+        //                console.log(a, b, c);
+        //                //alert()
+        //                $scope.selected = c
+        //                $scope.selected.show = true
+        //            }
+        //        }
+        //    });
+        //    console.log(data.results);
+        //    $scope.trips = data.results;
+        //    //$scope.map.center = data.results[0].pos
+        //    //console.log($scope.map.center);
+        //})
+    };
+    function setTrips (url) {
+        $http.get(url).success(function(data){
             console.log(data);
             data.results.forEach(function(v, i) {
                 v.pos = { latitude: v.location.coordinates[1], longitude: v.location.coordinates[0] }
@@ -29,12 +51,18 @@ foodApp.controller('mapCtrl', ['$scope', '$http', 'uiGmapGoogleMapApi', function
                     }
                 }
             });
-            console.log(data.results);
-            $scope.trips = data.results;
+            //console.log(data.results);
+            $scope.trips = $scope.trips.concat(data.results);
+            if(data.next) {
+                setTrips(data.next)
+            }
             //$scope.map.center = data.results[0].pos
             //console.log($scope.map.center);
         })
-    };
+        //console.log(data.results);
+        //$scope.trips = $scope.trips.concat();
+
+    }
     //$scope.selected = {};
     $scope.map = {
         center: { latitude: 19.1968401, longitude: 72.8710581 }, //initial center of map
