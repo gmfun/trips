2aab8ff588d53788c45eed0998affa9a54f1c0c9
gaurav <gaurav.mukherjee3@gmail.com>
2015-08-26 01:01:12 +0530

    start fix

--- js/mapCtrl.js
+++ js/mapCtrl.js
@@ -3,26 +3,10 @@ foodApp.controller('mapCtrl', ['$scope', '$http', 'uiGmapGoogleMapApi', function
 
     $scope.trips = [];
     $scope.getTrips = function(){
+        $scope.trips= [];
         var url = "https://hypertrack-api-staging.herokuapp.com/api/v1/gps/?trip_id=" + $scope.trip_id;
         setTrips(url)
-        //$http.get("https://hypertrack-api-staging.herokuapp.com/api/v1/gps/?trip_id=" + $scope.trip_id).success(function(data){
-        //    console.log(data);
-        //    data.results.forEach(function(v, i) {
-        //        v.pos = { latitude: v.location.coordinates[1], longitude: v.location.coordinates[0] }
-        //        v.events = {
-        //            click: function(a, b, c) {
-        //                console.log(a, b, c);
-        //                //alert()
-        //                $scope.selected = c
-        //                $scope.selected.show = true
-        //            }
-        //        }
-        //    });
-        //    console.log(data.results);
-        //    $scope.trips = data.results;
-        //    //$scope.map.center = data.results[0].pos
-        //    //console.log($scope.map.center);
-        //})
+
     };
     function setTrips (url) {
         $http.get(url).success(function(data){
@@ -40,6 +24,9 @@ foodApp.controller('mapCtrl', ['$scope', '$http', 'uiGmapGoogleMapApi', function
             });
             //console.log(data.results);
             $scope.trips = $scope.trips.concat(data.results);
+            if(!data.previous){
+                $scope.map.center = angular.copy(data.results[0].pos);
+            }
             if(data.next) {
                 setTrips(data.next)
             }
